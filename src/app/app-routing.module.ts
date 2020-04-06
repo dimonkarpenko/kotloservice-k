6573fb5f36a9865af0db2fdfee95a9b7f4f2f6a7
dima <134.2.zto@gmail.com>
2020-04-06 19:04:21 +0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ServicesComponent } from './services/services.component';
+import { MarksComponent } from './marks/marks.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 const routes: Routes = [
-  {path: 'services', component: ServicesComponent}
+  {path: 'services', component: ServicesComponent},
+  {path: 'marks', component: MarksComponent},
+  {path: 'services', component: ContactsComponent}
 ];
 
 @NgModule({
